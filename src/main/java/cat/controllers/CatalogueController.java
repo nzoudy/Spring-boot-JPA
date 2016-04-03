e361bbc861767872e9cf0e617e9145148632ad48
nzouda urbain <nzoudy@hotmail.fr>
2016-04-04 00:00:02 +0200

    add dao controller and restful

---
+++ src/main/java/cat/controllers/CatalogueController.java
@@ -0,0 +1,67 @@
+package cat.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cat.dao.IProduitRepository;
+import cat.entities.Produit;
+
+@RestController
+public class CatalogueController {
+	
+	@Autowired
+	private IProduitRepository produitRepository;
+
+	@RequestMapping("/test")
+	public String test(){
+		return "test";
+	}
+	
+	@RequestMapping("/save")
+	public Produit saveProduit(Produit p){
+		produitRepository.save(p);
+		return p;
+	}
+	
+	@RequestMapping("/all")
+	public List<Produit> getProduits(){
+		return produitRepository.findAll();
+	}
+	
+	@RequestMapping("/produits")
+	public Page<Produit> getProduits(int page){
+		return produitRepository.findAll(new PageRequest(page, 5));
+	}
+	
+	@RequestMapping("/produitsParMC")
+	public Page<Produit> getProduits(String mc, int page){
+		return produitRepository.produitParMC("%"+mc+"%", new PageRequest(page, 2));
+	}
+	
+	@RequestMapping("/get")
+	public Produit getProduit(Long ref){
+		return produitRepository.findOne(ref);
+	}
+
+	@RequestMapping("/delete")
+	public boolean delete(Long ref){
+		produitRepository.delete(ref);
+		return true;
+	}
+	
+	@RequestMapping("/update")
+	public Produit update(Produit p){
+		return produitRepository.saveAndFlush(p);
+		
+	}
+	
+	
+	
+}
