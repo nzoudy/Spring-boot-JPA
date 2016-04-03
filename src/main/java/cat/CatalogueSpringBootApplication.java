fd2110a6a331a0bb9155fab184cba37dfc8ed472
nzouda urbain <nzoudy@hotmail.fr>
2016-04-03 20:50:41 +0200

    first commit

---
+++ src/main/java/cat/CatalogueSpringBootApplication.java
@@ -0,0 +1,12 @@
+package cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueSpringBootApplication.class, args);
+	}
+}
